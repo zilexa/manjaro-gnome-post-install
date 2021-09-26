e661c61ac45eea31f2b45b773d8fa7cd59a39827
ZileXa <zilexa@gmx.com>
2021-09-26 11:31:10 +0200

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -18,3 +18,27 @@ File: /usr/share/applications/org.gnome.gedit.desktop
 replace gedit with pluma
 set Icon=org.gnome.pluma to Icon=org.gnome.gedit
 rename file to pluma.desktop
+
+
+dconf watch /  
+/org/nemo/preferences/quick-renames-with-pause-in-between
+  true
+
+/org/nemo/preferences/date-format
+  'iso'
+
+/org/nemo/preferences/show-reload-icon-toolbar
+  true
+
+/org/nemo/preferences/default-folder-viewer
+  'list-view'
+
+
+/org/nemo/preferences/inherit-folder-viewer
+  false
+(default)
+
+/org/gnome/desktop/peripherals/touchpad/natural-scroll
+  false
+
+
