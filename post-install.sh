25135bb5bfd5fc8962c2609a11f627da22f4a95f
ZileXa <zilexa@gmx.com>
2022-04-07 20:58:38 +0200

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -564,8 +564,8 @@ echo "--------------------------------------------------------------------------
 echo "Move documents folders to subvolume/username/ and link them back to $HOME except for /Downloads" 
 echo "Also simplify folder structure" 
 # Create Documents folder in subvolume because moving the folder does not seem to work
-#sudo mkdir -p /mnt/userdata/${USER}/Documents
-#sudo chown ${USER}:${USER} /mnt/userdata/${USER}/Documents
+sudo mkdir -p /mnt/userdata/${USER}
+sudo chown ${USER}:${USER} /mnt/userdata/${USER}/Documents
 ## Move personal user folders to the subvolume, rename Videos to Media while doing that
 sudo mv --verbose /home/${USER}/Documents /mnt/userdata/${USER}/
 sudo mv --verbose /home/${USER}/Music/ /mnt/userdata/${USER}/
