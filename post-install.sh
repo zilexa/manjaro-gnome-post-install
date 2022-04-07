8f2d145adb7bd4256537f6e27b39937a4095bb4f
ZileXa <zilexa@gmx.com>
2022-04-07 15:08:21 +0200

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -561,9 +561,9 @@ mv $HOME/Templates $HOME/Documents/
 sed -i -e 's+$HOME/Templates+$HOME/Documents/Templates+g' $HOME/.config/user-dirs.dirs
 
 ## Move personal user folders to the subvolume, rename Videos to Media while doing that
-mv /home/${USER}/Documents/ /mnt/userdata/${USER}
-mv /home/${USER}/Music/ /mnt/userdata/${USER}
-mv /home/${USER}/Pictures/ /mnt/userdata/${USER}
+mv /home/${USER}/Documents/ /mnt/userdata/${USER}/
+mv /home/${USER}/Music/ /mnt/userdata/${USER}/
+mv /home/${USER}/Pictures/ /mnt/userdata/${USER}/
 mv /home/${USER}/Videos /mnt/userdata/${USER}/Media
 
 ## Link personal folders inside subvolume back into home subvolume
