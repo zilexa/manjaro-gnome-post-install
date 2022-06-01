d63728b9794e1f0fb5f9046dcc78ac8e5208051e
ZileXa <zilexa@gmx.com>
2022-06-01 19:16:27 +0200

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -496,17 +496,22 @@ gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "[
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'Terminal'         
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'gnome-terminal'
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Ctrl><Alt>T' 
-# Remove Gnome default screenshot shortcuts as they will always be stored in /Pictures instead of /Pictures/Screenshots
+
+# Remove Gnome <41 (gnome-screenshots tool) default screenshot shortcuts, they are not logical and do not store screenshots in Pictures/Screenshots
 gsettings set org.gnome.settings-daemon.plugins.media-keys area-screenshot "@as []"
 gsettings set org.gnome.settings-daemon.plugins.media-keys window-screenshot "@as []"
 gsettings set org.gnome.settings-daemon.plugins.media-keys screenshot "@as []"
 gsettings set org.gnome.settings-daemon.plugins.media-keys area-screenshot-clip "@as []"
 gsettings set org.gnome.settings-daemon.plugins.media-keys window-screenshot-clip "@as []"
 gsettings set org.gnome.settings-daemon.plugins.media-keys screenshot-clip "@as []"
+# Remove Gnome 42> (Screenshots tool) default screenshot shortcuts, they use the new tool that requires too much user interaction
+gsettings set org.gnome.shell.keybindings screenshot "@as []"
+gsettings set org.gnome.shell.keybindings screenshot show-screenshot-ui "@as []"
+gsettings set org.gnome.shell.keybindings screenshot screenshot-window "@as []"
 # Create Screenshots folder
 mkdir $HOME/Pictures/Screenshots
 gsettings set org.gnome.gnome-screenshot auto-save-directory "file:///home/${USER}/Pictures/Screenshots/"
-# Create custom screenshot shortcuts, only custom shortcuts ARE stored to the auto-save-directory
+# Create custom screenshot shortcuts using foold old gnome-screenshots, only custom shortcuts ARE stored to the auto-save-directory
 gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom4/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom5/']"     
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ name 'Area screenshot to custom folder'
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ command 'gnome-screenshot -a'
